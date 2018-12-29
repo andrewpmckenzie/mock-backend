f9c27faf4ec68218d5998a08e04aacfdce84fa4d
Andrew McKenzie <andrewpmckenzie@gmail.com>
2018-12-29 12:13:08 -0500

    Add class names to status item elements.
    As hooks for test methods.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -23,13 +23,18 @@ export const App = ({requests, togglePause, handleNow}: AppProps) => (
       requests.length ?
           <StatusList>
             {requests.map((r) => (
-              <StatusListItem key={r.id}>
-                <StatusListItemDetails>{r.request.url}</StatusListItemDetails>
-                <Progress value={r.percentProgress} max={1} />
-                <RoundButton onClick={() => togglePause(r)}>
+              <StatusListItem className='StatusListItem' key={r.id}>
+                <StatusListItemDetails className='StatusListItem_details'>
+                  {r.request.url}
+                </StatusListItemDetails>
+                <Progress className='StatusListItem_progress'
+                          value={r.percentProgress} max={1} />
+                <RoundButton className='StatusListItem_togglePause'
+                             onClick={() => togglePause(r)}>
                   {r.handlingPaused ? <PlayArrow /> : <Pause />}
                 </RoundButton>
-                <RoundButton onClick={() => handleNow(r)}><SkipNext /></RoundButton>
+                <RoundButton className='StatusListItem_handleNow'
+                             onClick={() => handleNow(r)}><SkipNext /></RoundButton>
               </StatusListItem>
             ))}
           </StatusList>
